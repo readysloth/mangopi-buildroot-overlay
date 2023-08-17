d806e983692bd470f1653c22528d34000a966868
readysloth <readysloth@protonmail.com>
2023-08-17 15:42:01 +0300

    feat(package/datetime-restore/datetime-restore.mk): Added bootstrap date

--- package/datetime-restore/datetime-restore.mk
+++ package/datetime-restore/datetime-restore.mk
@@ -9,6 +9,7 @@ DATETIME_RESTORE_LICENSE = GPL-3.0+
 define DATETIME_RESTORE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m +rx -D $(DATETIME_RESTORE_PKGDIR)/datetime-restore $(TARGET_DIR)/etc/init.d/datetime-restore
 	ln --symbolic --relative $(TARGET_DIR)/etc/{init.d,runlevels/default}/datetime-restore
+	date +%s > $(TARGET_DIR)/etc/closest.date
 endef
 
 $(eval $(generic-package))
