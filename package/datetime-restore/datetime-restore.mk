e85f662087b7d5f63b85dd3549f2692ffedd83ab
readysloth <readysloth@protonmail.com>
2023-08-17 11:44:18 +0300

    fix(package/*/*): Fixes for build and install

--- package/datetime-restore/datetime-restore.mk
+++ package/datetime-restore/datetime-restore.mk
@@ -7,7 +7,7 @@
 DATETIME_RESTORE_LICENSE = GPL-3.0+
 
 define DATETIME_RESTORE_INSTALL_TARGET_CMDS
-	install -m +x -D $(@D)/datetime-restore $(TARGET_DIR)/etc/init.d/datetime-restore
+	$(INSTALL) -m +rx -D $(DATETIME_RESTORE_PKGDIR)/datetime-restore $(TARGET_DIR)/etc/init.d/datetime-restore
 	ln --symbolic --relative $(TARGET_DIR)/etc/{init.d,runlevels/default}/datetime-restore
 endef
 
--- package/telegram-cctv/telegram-cctv.mk
+++ package/telegram-cctv/telegram-cctv.mk
@@ -5,13 +5,13 @@
 ################################################################################
 
 TELEGRAM_CCTV_LICENSE = GPL-3.0+
-TELEGRAM_CCTV_VERSION = master
-TELEGRAM_CCTV_SITE_ = git@github.com:readysloth/telegram-cctv.git
+TELEGRAM_CCTV_VERSION = 283d7c4f2efd27ddc1f451bc0d6f806313f65856
+TELEGRAM_CCTV_SITE = git@github.com:readysloth/telegram-cctv.git
 TELEGRAM_CCTV_SITE_METHOD = git
 TELEGRAM_CCTV_CONFIGURE_OPTS = CFLAGS='$(TARGET_CFLAGS) -DTG_BOT_TOKEN="\"${TG_CCTV_TOKEN}\"" -DTG_CHANNEL="\"${TG_CCTV_CHANNEL}\""'
 
 define TELEGRAM_CCTV_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(TELEGRAM_CCTV_CONFIGURE_OPTS) -C $(@D)
 endef
 
 define TELEGRAM_CCTV_INSTALL_TARGET_CMDS
