151b8bbd2137a172da72a1c22f9a923f9481ce42
readysloth <readysloth@protonmail.com>
2023-08-19 15:15:22 +0300

    fix(package/*): Fixed configs and services

--- package/alcatel-dongle/alcatel-dongle.mk
+++ package/alcatel-dongle/alcatel-dongle.mk
@@ -8,6 +8,7 @@ ALCATEL_DONGLE_LICENSE = GPL-3.0+
 
 define ALCATEL_DONGLE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m +rx -D $(ALCATEL_DONGLE_PKGDIR)/alcatel-dongle $(TARGET_DIR)/etc/init.d/alcatel-dongle
+	ln --symbolic --relative $(TARGET_DIR)/etc/{init.d,runlevels/default}/alcatel-dongle
 
 	$(INSTALL) -m +r -D $(ALCATEL_DONGLE_PKGDIR)/alcatel-conf/hostapd.conf $(TARGET_DIR)/etc/
 	$(INSTALL) -m +r -D $(ALCATEL_DONGLE_PKGDIR)/alcatel-conf/tinyproxy.conf $(TARGET_DIR)/etc/tinyproxy/
